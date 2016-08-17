6a1f29ad9629dfdfd471c4efad3b87fd33d45217
alexalegor <info@calypto.se>
2016-08-17 15:47:55 +0200

    Changing PartnerId variable from nummeric to string

--- lib/paysonmerchant.php
+++ lib/paysonmerchant.php
@@ -13,7 +13,7 @@ namespace PaysonEmbedded {
         public $termsUri;
         /** @var string $reference Merchants own reference of the checkout.*/
         public $reference = NULL;
-        /** @var int $partnerId Partners unique identifier */
+        /** @var string $partnerId Partners unique identifier */
         public $partnerId = NULL;
         /** @var string $integrationInfo Information about the integration. */
         public $integrationInfo = NULL;
