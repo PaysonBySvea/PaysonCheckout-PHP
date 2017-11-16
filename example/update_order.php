963583ca23ecb6de31de5b7b8cb5c7ea0a35c05a
oscar-vil <oscar.villegas@payson.se>
2017-11-16 11:30:00 +0100

    Fixing BankId.

--- example/update_order.php
+++ example/update_order.php
@@ -9,7 +9,7 @@ $payData->AddOrderItem(new  PaysonEmbedded\OrderItem('Test product', 500, 1, 0.2
 
 
 $customer = new  PaysonEmbedded\Customer('firstName', 'lastName', 'email', 'phone', 'identityNumber', 'city', 'country', 'postalCode', 'street');
-$gui = new  PaysonEmbedded\Gui('sv', 'gray', 'none', 0 /*, ['SE', 'GB']*/);
+$gui = new  PaysonEmbedded\Gui('sv', 'gray', 0, 0 /*, ['SE', 'GB']*/);
 $checkout = new  PaysonEmbedded\Checkout($paysonMerchant, $payData, $gui,$customer);
 
 // Update the purchase with the checkoutID.
--- lib/gui.php
+++ lib/gui.php
@@ -5,8 +5,8 @@ namespace PaysonEmbedded{
         public $colorScheme;
         /** @var string $locale Used to change the language shown in the checkout snippet ("se", "fi", "en" (default)). */
         public $locale;
-        /** @var string $verfication  Can be used to add extra customer verfication ("bankid", "none" (default)). */
-        public $verfication;
+        /** @var string $verification  Can be used to add extra customer verfication ("bankid", "none" (default)). */
+        public $verification;
         /** @var bool $requestPhone  Can be used to require the user to fill in his phone number. */
         public $requestPhone;
         /** @var list $countries  List of countries shown in the checkout snippet. */
@@ -14,10 +14,10 @@ namespace PaysonEmbedded{
         /** @var bool $phoneOptional  Can be used to ask the user to fill in his phone number, but not strict required. */
         public $phoneOptional;
         
-        public function __construct($locale = "sv", $colorScheme = "gray", $verfication = "none", $requestPhone = NULL, $countries = NULL, $phoneOptional = NULL){
+        public function __construct($locale = "sv", $colorScheme = "gray", $verification = 0, $requestPhone = NULL, $countries = NULL, $phoneOptional = NULL){
             $this->colorScheme = $colorScheme;
             $this->locale = $locale; 
-            $this->verfication = $verfication;
+            $this->verification = $verification;
             $this->requestPhone = $requestPhone;
             $this->countries = $countries;
             $this->phoneOptional = $phoneOptional;
